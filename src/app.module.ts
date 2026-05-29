9956cd317649907eaf85312d4faa36fd4fdbf29b
Mayun Suryatama <mayunngrh@gmail.com>
2026-05-29 14:05:22 +0800

    update the connection url into a solid one

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,11 +14,7 @@ import { Tag } from './entities/tag.entity';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT ?? '5432'),
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
+      url: process.env.DATABASE_URL,
       entities: [Place, Tag],
       synchronize: true,
       logging: true,
