18b6817cf8b7d963e85cd902255d06f7e496c4b1
Mayun Suryatama <mayunngrh@gmail.com>
2026-06-05 13:49:16 +0800

    change the var name back into photoReference

--- src/plan/plan.service.ts
+++ src/plan/plan.service.ts
@@ -132,7 +132,7 @@ export class PlanService {
               regency: item.place.regency,
               province: item.place.province,
               description: item.place.description,
-              photoUrl: item.place.photoReference
+              photoReference: item.place.photoReference
                 ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=800&photo_reference=${item.place.photoReference}&key=${apiKey}`
                 : null,
               latitude: parseFloat(item.place.latitude as any),
